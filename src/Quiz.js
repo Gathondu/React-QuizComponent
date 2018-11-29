36f90451010854a11d38457d40f8ec070a1ed4ec
Denis Ngugi Gathondu <thundoss@gmail.com>
2018-11-29 21:18:05 +0300

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,29 +6,35 @@ let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
     constructor(props) {
-        super(props)
-        this.state = {quiz_position: 1}
+        super(props);
+        this.state = { quiz_position: 1 };
     }
-    showNextQuestion () {
+    showNextQuestion() {
         this.setState((prevState, props) => {
-            return { quiz_position: prevState.quiz_position + 1 }
-        })
+            return { quiz_position: prevState.quiz_position + 1 };
+        });
+    }
+    handleResetClick() {
+        this.setState(({ quiz_position: 1 }))
     }
 
     render() {
-        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
-        return <div>
-            {isQuizEnd ? (
-              <QuizEnd />
-            ) : (
-              <QuizQuestion
-                showNextQuestionHandler={this.showNextQuestion.bind(this)}
-                quiz_question={
-                  quizData.quiz_questions[this.state.quiz_position - 1]
-                }
-              />
-            )}
-          </div>;
+        const isQuizEnd =
+            this.state.quiz_position - 1 === quizData.quiz_questions.length;
+        return (
+            <div>
+                {isQuizEnd ? (
+                    <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
+                ) : (
+                        <QuizQuestion
+                            showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                            quiz_question={
+                                quizData.quiz_questions[this.state.quiz_position - 1]
+                            }
+                        />
+                    )}
+            </div>
+        );
     }
 }
 
